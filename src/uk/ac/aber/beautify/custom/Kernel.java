8574671cd7e0ed53a78ba3c025a1624417448527
jotask <jota_sk@hotmail.com>
2015-11-29 15:16:57 +0000

    JavaDocs on kernel added

--- src/uk/ac/aber/beautify/custom/Kernel.java
+++ src/uk/ac/aber/beautify/custom/Kernel.java
@@ -1,33 +1,76 @@
-package uk.ac.aber.beautify.custom.filter;
+package uk.ac.aber.beautify.custom;
 
 /**
- * Created by Jose Vives on 17/11/2015.
+ * Kernel class for build kernels.
+ *
+ * This class can generate kernels for few type of kernels
  *
  * @author Jose Vives.
  * @since 17/11/2015
  */
 public class Kernel {
 
+    /**
+     * The kernel we generate
+     */
     private double[][] kernel;
 
+    /**
+     * Constructor for this class
+     * Initialise the kernel variable with the size we want
+     * This is for a square kernnel
+     *
+     * @param size
+     *      The size for the kernel we want
+     */
     public Kernel(int size) {
-        kernel = new double[size][size];
+        this(size, size);
     }
 
+    /**
+     * Constructor for intialise the kernel
+     *
+     * This method can create a no squared kernel
+     *
+     * @param width
+     *      The width of the kernel we want build
+     * @param height
+     *      The height of the kernel we wan build
+     */
+    public Kernel(int width, int height){
+        kernel = new double[width][height];
+    }
+
+    /**
+     * Get the with of the actual kernel
+     * @return
+     *  The width of the kernel
+     */
     public int getWidth(){
         return kernel.length;
     }
 
+    /**
+     * Get the height of the kernel
+     * @return
+     *      The height of the kernel
+     */
     public int getHeight(){
         return kernel[0].length;
     }
 
+    /**
+     * Set this kernel to a normal distribution kernel
+     */
     public void setNormalKernel(){
         for(int i = 0; i < getWidth(); i++)
             for(int j = 0; j < getHeight(); j++)
                 kernel[i][j] = (1f / (getWidth() * getHeight()));
     }
 
+    /**
+     * For debug purposes. This print the actual kernel
+     */
     public void printKernel(){
         StringBuilder builder = new StringBuilder();
         System.out.println("Width: " + getWidth() + " Height: " + getHeight());
@@ -43,9 +86,15 @@ public class Kernel {
         System.out.println(builder.toString());
     }
 
+    /**
+     * Set this kernel to be a gaussian kernel
+     *
+     * All the values for the kernel is calcualted from the size of the kernel
+     *
+     */
     public void setGaussian() {
 
-        int radius = getRadius();
+        int radius = getFilterWidth();
 
         double[][] k = new double[radius * 2 + 1][radius * 2 + 1];
         double total = 0.0;
@@ -74,20 +123,35 @@ public class Kernel {
 
     }
 
-    public int getRadius(){
-        return (getWidth() - 1) / 2;
-    }
-
+    /**
+     * Get the radio width of the actual kernel
+     * @return
+     *      The radio width of the actual kernel
+     */
     public int getFilterWidth(){
         return (getWidth() - 1) / 2;
     }
 
+    /**
+     * Get the radio height of the actual kernel
+     * @return
+     *      The radio height of the actual kernel
+     */
     public int getFilterHeight(){
         return (getHeight() - 1) / 2;
     }
 
-    public double getValue(int i, int j){
-        return this.kernel[i][j];
+    /**
+     * Get the value in position i and j from the actual kernel
+     * @param x
+     *      The i position
+     * @param y
+     *      The y position
+     * @return
+     *      the value of that position in kernel
+     */
+    public double getValue(int x, int y){
+        return this.kernel[x][y];
     }
 
 }
