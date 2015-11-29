182eec29be9ad029616cac5f587aefd73c2f43ca
jotask <jota_sk@hotmail.com>
2015-11-29 20:41:32 +0000

    Added comments to the basic algorithm

--- src/uk/ac/aber/beautify/custom/Jose.java
+++ src/uk/ac/aber/beautify/custom/Jose.java
@@ -22,32 +22,50 @@ public class Jose extends Filter{
     @Override
     public BufferedImage filter(BufferedImage input) {
 
+        // Create a copy from the original image for work on this
+        // new image
         BufferedImage output = BeautifyUtils.getCopy(input);
 
+        // Firstly we sharp the image using a gaussian filter, and
+        // later a unsharp mask filter
         output = FilterConvolution.unsharpMaskFilter(output, 15, 2.0);
+
+        // Start the point pixel value operations
 
         WritableRaster raster = output.getRaster();
 
+        // Loop all the pixels
         for(int u = 0; u < raster.getWidth(); u++){
             for(int v = 0; v < raster.getHeight(); v++){
 
+                // Get rgb values from the current pixel
                 double[] rgb = new double[3];
-
                 raster.getPixel(u, v, rgb);
 
+                // Convert from rgb to lab
                 double[] lab = BeautifyUtils.RGBtoLAB(rgb);
 
+                // Decrease the Lightness
                 lab[0] += -5;
+
+                // The a and b values are the color-component dimension and
+                // I find that adding 10 to the b channel makes a sepia color
+                // effect
                 lab[2] += 10;
 
+                // Convert LAB value to RGB values
                 rgb = BeautifyUtils.LABtoRGB(lab);
 
+                // Just for safety properties clamp the RGG values
                 rgb = BeautifyUtils.clamp(rgb);
+
+                // Set this to the result image
                 raster.setPixel(u, v, rgb);
 
             }
         }
 
+        // Blending this image with a overlay image for make a the border darker
         output = Blending.multiply(output, 0.75);
 
         return output;
